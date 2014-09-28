64b619e9811ddde73a5d1a79ace4a6dc1b377d4a
Keith Chambers <kechambe@cisco.com>
2014-09-27 19:50:55 -0700

    Added build.sh for quick easy building with mock.

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TOP_DIR=${PWD}
+
+rm -rf "${TOP_DIR}/RPMS"
+rm -rf "%{TOP_DIR}/SRPMS"
+
+mock --buildsrpm \
+        --verbose \
+        --define "dist .el7" \
+        --resultdir="${TOP_DIR}/SRPMS" \
+        --spec ${TOP_DIR}/SPECS/*.spec \
+        --sources "${TOP_DIR}/SOURCES"
+
+mock --rebuild \
+        --verbose \
+        --define "dist .el7" \
+        --resultdir="${TOP_DIR}/RPMS" \
+        --define "commit ${COMMIT}" \
+        ${TOP_DIR}/SRPMS/*.src.rpm
+
+# EOF
